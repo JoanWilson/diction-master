f774296714c0025172d8bb906b048a7e2c94b7d6
Joan Wilson <joanwmoliveira@gmail.com>
2024-02-03 00:15:18 -0300

    feat: add Data Layer

---
+++ DataTests/DataTests.swift
@@ -0,0 +1,21 @@
+//
+//  DataTests.swift
+//  DataTests
+//
+//  Created by Joan Wilson Oliveira on 03/02/24.
+//
+
+import XCTest
+@testable import Data
+
+final class DataTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
