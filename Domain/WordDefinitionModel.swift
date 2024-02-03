a8e88b7430acfe9eb5b63132199ece2aeef1b838
Joan Wilson <joanwmoliveira@gmail.com>
2024-02-03 00:54:13 -0300

    feat: remove old domain and data layer

--- Domain/WordDefinitionModel.swift
+++ Domain/WordDefinitionModel.swift
@@ -13,6 +13,14 @@ public struct WordDefinitionModel {
     public let phonetics: [Phonetic]
     public let origin: String
     public let meanings: [Meaning]
+    
+    public init(word: String, phonetic: String, phonetics: [Phonetic], origin: String, meanings: [Meaning]) {
+        self.word = word
+        self.phonetic = phonetic
+        self.phonetics = phonetics
+        self.origin = origin
+        self.meanings = meanings
+    }
 }
 
 public struct Phonetic: Codable {
